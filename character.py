70a4a00089fde3a9b918e3f185cf1823d0fd48b8
Spring Cheng <scheng148@my.bcit.ca>
2024-04-09 20:28:38 -0700

    Update identifiers for consistency across modules

--- character.py
+++ character.py
@@ -17,32 +17,32 @@ def make_character():
     :postcondition: create a dictionary with keys "time", "GPA", "social", and "location"
     :return: character attributes "time," "GPA," "social" and "location" as a dictionary
 
-    >>> test_character = make_character()
-    >>> test_character
+    >>> test_player = make_character()
+    >>> test_player
     {'time': 100, 'GPA': 3.5, 'social': 50, 'location': 1}
     """
     return {'time': 100, 'GPA': 3.5, 'social': 50, 'location': 1}
 
 
-def make_character_each_term_start(character):
+def make_character_each_term_start(player):
     """
     Create a dictionary that contains character's attributes at the start of each term that corresponds to that level.
 
     :postcondition: create a dictionary with keys "time", "GPA", "social", and "location"
     :return: character attributes "time," "GPA," "social" and "location" as a dictionary
 
-    >>> test_character = {'time': 50, 'GPA': 3.8, 'social': 50, 'location': 26}
-    >>> result = make_character_each_term_start(test_character)
+    >>> test_player = {'time': 50, 'GPA': 3.8, 'social': 50, 'location': 26}
+    >>> result = make_character_each_term_start(test_player)
     >>> result
     {'time': 120, 'GPA': 3.5, 'social': 60, 'location': 26}
     """
-    if character['location'] == 26:
-        character = {'time': 120, 'GPA': 3.5, 'social': 60, 'location': 26}
-    elif character['location'] == 51:
-        character = {'time': 150, 'GPA': 3.5, 'social': 70, 'location': 51}
-    elif character['location'] == 76:
-        character = {'time': 180, 'GPA': 3.5, 'social': 80, 'location': 76}
-    return character
+    if player['location'] == 26:
+        player = {'time': 120, 'GPA': 3.5, 'social': 60, 'location': 26}
+    elif player['location'] == 51:
+        player = {'time': 150, 'GPA': 3.5, 'social': 70, 'location': 51}
+    elif player['location'] == 76:
+        player = {'time': 180, 'GPA': 3.5, 'social': 80, 'location': 76}
+    return player
 
 
 def check_for_instructors():
@@ -56,25 +56,25 @@ def check_for_instructors():
     return chance_of_encounter <= 0.25
 
 
-def check_if_graduated_this_term(character):
+def check_if_graduated_this_term(player):
     """
     Check if the goal of meeting term graduation criteria is attained.
 
-    :param character: a dictionary that stores the character's attributes
-    :precondition: character must be a dictionary with keys "time", "GPA", "social", and "location"
+    :param player: a dictionary that stores the character's attributes
+    :precondition: player must be a dictionary with keys "time", "GPA", "social", and "location"
     :postcondition: check if player's character attributes has reached the criteria for term graduation:
                  - 'GPA': greater than or equal to 2.0
     :return: if player's character attributes has met graduation criteria or not as a bool
 
-    >>> test_character = {'time': 50, 'GPA': 3.5, 'social': 50, 'location': 75}
-    >>> check_if_graduated_this_term(test_character)
+    >>> test_player = {'time': 50, 'GPA': 3.5, 'social': 50, 'location': 75}
+    >>> check_if_graduated_this_term(test_player)
     True
 
-    >>> test_character = {'time': 50, 'GPA': 1.0, 'social': 50, 'location': 25}
-    >>> check_if_graduated_this_term(test_character)
+    >>> test_player = {'time': 50, 'GPA': 1.0, 'social': 50, 'location': 25}
+    >>> check_if_graduated_this_term(test_player)
     False
     """
-    if character['GPA'] >= 2.0:
+    if player['GPA'] >= 2.0:
         return True
     else:
         return False
@@ -89,21 +89,21 @@ def quiz_question():
     return questions[0]
 
 
-def is_enrolled(character):
+def is_enrolled(player):
     """
     Evaluate if the player is still enrolled in the program as a student.
 
-    :param character: a dictionary that stores the character's coordinates and GPA
-    :precondition: character must be a dictionary with the format "X-coordinate": 0, "Y-coordinate": 0, "GPA": 2.0
+    :param player: a dictionary that stores the character's coordinates and GPA
+    :precondition: player must be a dictionary with the format "X-coordinate": 0, "Y-coordinate": 0, "GPA": 2.0
     :postcondition: evaluate if the player's HP is still enrolled in the program as a student
     :return: if the player's GPA has dipped below 2.0 or not as a bool
 
-    >>> test_character = {'time': 50, 'GPA': 1.0, 'social': 50, 'location': 25}
-    >>> is_enrolled(test_character)
+    >>> test_player = {'time': 50, 'GPA': 1.0, 'social': 50, 'location': 25}
+    >>> is_enrolled(test_player)
     False
 
-    >>> test_character = {'time': 50, 'GPA': 3.5, 'social': 50, 'location': 75}
-    >>> is_enrolled(test_character)
+    >>> test_player = {'time': 50, 'GPA': 3.5, 'social': 50, 'location': 75}
+    >>> is_enrolled(test_player)
     True
     """
-    return character['GPA'] >= 2.0
+    return player['GPA'] >= 2.0
